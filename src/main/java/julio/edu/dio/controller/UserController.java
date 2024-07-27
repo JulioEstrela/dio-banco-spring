feceef760a1131e3395a240b62d3516bc48b680a
Julio Estrela <julioantunesestrela13@gmail.com>
2024-07-27 19:35:14 -0300

    docs: improve UserController documentation
    using OpenApi

--- src/main/java/julio/edu/dio/controller/UserController.java
+++ src/main/java/julio/edu/dio/controller/UserController.java
@@ -1,5 +1,10 @@
 package julio.edu.dio.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import julio.edu.dio.domain.model.User;
 import julio.edu.dio.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -9,7 +14,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+@Tag(name = "Controle de usuários", description = "Gerenciamento de usuários")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,16 +27,28 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Retorna todos os usuários")
+    @ApiResponse(responseCode = "200")
     public ResponseEntity<List<User>> getAll() {
         return ResponseEntity.ok(userService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retorna um usuário pelo ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.findById(id));
     }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Cria um usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201"),
+            @ApiResponse(responseCode = "422", description = "Dados fornecidos são inválidos")
+    })
     public ResponseEntity<User> postUser(@RequestBody User userToCreate) {
         User userCreated = userService.create(userToCreate);
         URI userCreatedUri = ServletUriComponentsBuilder.fromCurrentRequest()
